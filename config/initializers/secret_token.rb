7529b16b6e94e0b115fd7c47472e7f6549d8102e
Robert Martin <robertmartinbc@shaw.ca>
2014-04-26 20:37:36 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BelleIsleMusic::Application.config.secret_token = '631142185b45cc5d3748ae3b269e9997e5a466fe08f476ebe8b2c30f003acafaf7a024bbf1713b3753412893143278b84524d19021fee1d1dc475ea1d12608d1'
